4b9c29e11eb047fc3fb41d246004223fb2456b0a
Sobia Furqan <sobiafurqan@hotmail.com>
2021-03-29 16:39:53 +0100

    adding these for task 4

--- collection_app.php
+++ collection_app.php
@@ -5,8 +5,22 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare("SELECT `Kdrama`, `ReleaseYear`, `Synopsis`, `Starring` FROM `Kdramas`;");
 $query->execute();
 $Kdramas = $query->fetchAll();
+
+?>
+
+<html lang="en">
+<head>
+    <title>Kdrama Collection App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
+
+    <?php
 echo '<ul>';
 foreach ($Kdramas as $Kdrama) {
     echo '<li>' . $Kdrama['Kdrama'] . ' - ' . $Kdrama['ReleaseYear'] . ' - ' . $Kdrama['Synopsis'] . ' - ' . $Kdrama['Starring'] . '</li>';
 }
 echo '</ul>';
+?>
+
+</html>
